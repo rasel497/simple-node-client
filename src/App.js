f29873d30098b40e815f2529a268f4a5ebcac134
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-30 02:37:45 +0600

    Create React form and Post API and send data to the server

--- src/App.js
+++ src/App.js
@@ -10,10 +10,41 @@ function App() {
       .then(data => setUser(data))
   }, [])
 
+  //hnadleAddNewUser
+  const hnadleAddNewUser = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch('http://localhost:5000/users/', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json)()
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+
+    form.reset();
+
+  }
+
+
   return (
     <div className="App">
       <h2>User: {users.length}</h2>
 
+      <form onSubmit={hnadleAddNewUser}>
+        <input name='name' type="text" placeholder='your name' /> <br />
+        <input name='email' type="text" placeholder='your email' /> <br />
+        <button>Add user</button>
+      </form>
       <div className="l">
         {
           users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
