f9921833433ad874c6ff3c7a28cf3ea08de7e9aa
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-29 23:15:59 +0600

    initial and using server api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users/')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>User: {users.length}</h2>
+
+      <div className="l">
+        {
+          users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        }
+      </div>
+
     </div>
   );
 }
